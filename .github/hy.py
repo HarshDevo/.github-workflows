0428ea1eb1490a46faa4c62d18f3dea251034f40
HarshDevo <137371243+HarshDevo@users.noreply.github.com>
2023-12-11 08:44:05 +0000

    Update hy.py

--- .github/hy.py
+++ .github/hy.py
@@ -1,1 +1,9 @@
-print("hello")
+import os
+
+folder1 = os.environment.get("FOLDER1")
+folder2 = os.environment.get("FOLDER2")
+folder3 = os.environment.get("FOLDER3")
+
+print(f"Folder 1: {folder1}")
+print(f"Folder 2: {folder2}")
+print(f"Folder 3: {folder3}")
